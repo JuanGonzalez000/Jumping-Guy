d0f81a9fee2dca91cad7c20dd83e57a469de85f9
Juan <juangonzalezb2@outlook.com>
2020-11-21 14:54:28 -0300

    Primera version

---
+++ Assets/Scripts/EnemyGeneratorController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGeneratorController : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public float generatorTimer = 1.75f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //Crea enemigos
+    void CreateEnemy()
+    {
+        Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+    }
+
+    //Inicia el generador de enemigos
+    public void StartGenerator()
+    {
+        InvokeRepeating("CreateEnemy", 0f, generatorTimer);
+    }
+
+    //Detiene el generador de enemigos
+    public void CancelGenerator()
+    {
+        CancelInvoke("CreateEnemy");
+    }
+}
